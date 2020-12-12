9b5699e3ecb183d169fcba78c4d8b86022ac841b
tmaniak85 <tomasz.maniak.85>
2020-12-12 09:20:23 +0100

    Dodatkowy plik

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello World!");
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("Hello World111!");
         }
     }
 }
